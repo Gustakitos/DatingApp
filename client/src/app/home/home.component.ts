23bcc37029827a155a7d247974d9826d35e7c042
Luis Gustavo <luisgustavolg1033@gmail.com>
2023-05-06 20:02:21 -0300

    End of section 9

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,28 +1,17 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
   users: any;
 
-  constructor(private http: HttpClient) { }
+  constructor() {}
 
-  ngOnInit(): void {
-    this.getUsers();
-  }
-
-  getUsers() {
-    this.http.get('http://localhost:5251/api/users').subscribe({
-      next: (response) => (this.users = response),
-      error: (error) => console.log('getUsers err: ', error),
-      complete: () => console.log('request completed'),
-    });
-  }
+  ngOnInit(): void {}
 
   registerToggle() {
     this.registerMode = !this.registerMode;
